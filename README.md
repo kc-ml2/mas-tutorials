ceb158da66b9dcc1cfdda123a5f0ce9421aef61a
ML2jinmyoung <mys1458@kc-ml2.com>
2022-07-08 14:30:19 +0900

    [ADD] google analytics

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 </p>
 <br>
 
-## 🚀 MAS TUTORIAL 소개
+# 🚀 MAS TUTORIAL 소개
 
 **MAS TUTORIAL**은 머신러닝 연구소 [KC-ML2](https://www.kc-ml2.com/)에서 제작한 '멀티에이전트 강화학습(MARL)' 튜토리얼입니다.
 본 튜토리얼은 다음과 같은 내용을 다루고 있습니다.
@@ -13,12 +13,12 @@
 
 ---
 
-## 🗒️ MAS TUTORIAL 내용 수정하기
+# 🗒️ MAS TUTORIAL 내용 수정하기
 
 튜토리얼에서 오타 또는 혼동되는 내용이나 개선을 위해 추가하실 내용이 있다면 issue를 올리시거나 PR을 해주세요!
 더욱 유익한 튜토리얼을 위한 여러분들의 참여를 기다립니다 : )
 
-#### PULL REQUEST 가이드라인
+### PULL REQUEST 가이드라인
 
 - ✅ 새로운 branch를 만들어 주세요!
   - 현재 사용되는 기본 branch (ex. `main`)가 아닌 별도의 branch를 새로 만들어 작업해 주세요.
@@ -27,7 +27,7 @@
 - ✅ 리뷰 후 추가적인 커밋이 생겼다면, `Re-request review`를 진행해 주세요!
   - 자세한 방법은 [문서](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/requesting-a-pull-request-review)를 참고해 주세요.
 
-#### Devcontainer 사용
+### Devcontainer 사용
 
 - Visual Studio Code에서 Docker 컨테이너를 개발 환경으로 사용하실 수 있습니다.
 - Visaul Studio Code에서 `Remote-Container: Reopen in Container` 커맨드를 실행하시거나, 하단의 `Reopen in Container` 버튼을 누르신 후 사용 가능합니다.
--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import NavBar from './NavBar'
 import Footer from './Footer'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 // eslint-disable-next-line react/prop-types
 export default function Layout({ children }) {
@@ -21,8 +22,21 @@ export default function Layout({ children }) {
                 <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
                 {/* code theme  */}
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-one-light.css" rel="stylesheet" />
-                {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" /> */}
+                {/* Global site tag (gtag.js) - Google Analytics  */}
+                <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                    `,
+                    }}
+                />
             </Head>
             <NavBar />
             <main>{children}</main>
